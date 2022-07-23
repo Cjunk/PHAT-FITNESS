c28d876904fe6b4982b322ba92ee55c203742fe2
Jericho Sharman <jsharman@hotmail.com.au>
2022-07-23 18:50:41 +1000

    init

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.get('/api/customers', cors(), (req, res) => {
+  const customers = [
+    {id: 1, firstName: 'John', lastName: 'Doe'},
+    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+  ];
+
+  res.json(customers);
+});
+
+const port = 5000;
+
+console.log(`Server running on port ${port}`)
+app.listen(port, () => `Server running on port ${port}`);
