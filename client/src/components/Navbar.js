56a3513e6aa4dc245696e932451837200061866a
Jericho Sharman <jsharman@hotmail.com.au>
2022-07-26 18:08:50 +1000

    update

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+// import { React } from 'react'
+var React = require("react");
+function Navbar() {
+    return (<div></div>);
+}
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+// import { React } from 'react'
+import React = require("react")
+function Navbar() {
+    return (
+        <div></div>
+    )
+}
