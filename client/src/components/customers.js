6899e2dbd5727cf2c431896b682c28f3e32bf572
Jericho Sharman <jsharman@hotmail.com.au>
2022-08-03 19:57:53 +1000

    Fixed api REACT error

--- client/src/components/customers.js
+++ client/src/components/customers.js
@@ -12,18 +12,10 @@ class Customers extends Component {
  
   }
   componentDidMount() {
-    console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    // fetch('/api/customers')
-    //   .then((res) => {
-    //     console.log("am here",res); res.json() })
-    //   .then((customers) => {
-    //     console.log("customers", customers);
-    //     this.setState({ customers })
-    //   });
     fetch('/api/customers',).then(res =>  res.json())
       .then(customers => {
         this.setState({ customers });  
-        console.log(customers)
+        // console.log(customers)
       })
       // 
   }
